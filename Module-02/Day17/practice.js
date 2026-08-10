b81c3da7cf944cec37853543ea3180bf9656d81e
Eyasu Yebeltal <joshgreaterthan@gmail.com>
2026-08-10 14:34:31 +0300

    Day 17

--- Module-02/Day17/practice.js
+++ Module-02/Day17/practice.js
@@ -1,5 +1,17 @@
 function greet(name) {
-    return "Hello, " + name + "!";
+  return "Hello, " + name + "!";
 }
 
 console.log(greet("Josh"));
+
+function outerFunction() {
+  let message = "Welcome to JavaScript!";
+
+  function innerFunction() {
+    console.log(message);
+  }
+
+  innerFunction();
+}
+
+outerFunction();
