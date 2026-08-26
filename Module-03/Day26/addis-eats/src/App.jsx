d6a1ce3f4659bd77e4a437fa1dfdce4b8e926650
Eyasu Yebeltal <joshgreaterthan@gmail.com>
2026-08-26 23:57:32 +0300

    Update

---
+++ Module-03/Day26/addis-eats/src/App.jsx
@@ -0,0 +1,14 @@
+import Dish from "./Dish";
+
+function App() {
+  return (
+    <div>
+      <h1>Addis Eats</h1>
+      <h2>Menu</h2>
+
+      <Dish name="Doro Wot" price={450} />
+    </div>
+  );
+}
+
+export default App;
